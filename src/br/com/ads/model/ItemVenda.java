aebd90609675f8e3e25ecbf0ca650d26207f8172
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-01-31 23:52:42 -0300

    Criando as Classes de Modelos de Vendas e ItemVendas

---
+++ src/br/com/ads/model/ItemVenda.java
@@ -0,0 +1,52 @@
+package br.com.ads.model;
+
+
+public class ItemVenda {
+
+    private int id;
+    private Vendas venda;
+    private Produtos produto;
+    private int qtd;
+    private double subTotal;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Vendas getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Vendas venda) {
+        this.venda = venda;
+    }
+
+    public Produtos getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produtos produto) {
+        this.produto = produto;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(double subTotal) {
+        this.subTotal = subTotal;
+    }
+    
+}
